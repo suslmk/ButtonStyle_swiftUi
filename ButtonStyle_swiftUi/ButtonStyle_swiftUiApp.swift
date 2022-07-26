ada3d9e1476dddd3fac3c28f1ed06f8fef3d1bcc
suslmk <67575226+suslmk-lee@users.noreply.github.com>
2022-07-27 05:07:21 +0900

    Initial Commit

---
+++ ButtonStyle_swiftUi/ButtonStyle_swiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonStyle_swiftUiApp.swift
+//  ButtonStyle_swiftUi
+//
+//  Created by minkyuLee on 2022/07/27.
+//
+
+import SwiftUI
+
+@main
+struct ButtonStyle_swiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ButtonStyle_swiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonStyle_swiftUi
+//
+//  Created by minkyuLee on 2022/07/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
